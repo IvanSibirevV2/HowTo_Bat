45729e3f4debf866c91946fee6fd0788b664bb73
qwe <qwe>
2021-11-01 19:13:04 +0300

    рефакторинг

---
+++ Base_L0/000_2_ПеременныхСреды.bat
@@ -0,0 +1,20 @@
+@echo off
+if "%cd%" EQU "C:\Program Files\Notepad++" (
+	cd C:\D\Git_Hub\HowTo_Bat\Base_L0
+)
+echo %cd%
+echo ::::::::::::::::::::::::::::::::::::::::::::::::
+rem Чтобы добавить переменную, в командной строке введите
+set FileWrite="%date% %time%.html"
+if defined FileWrite (echo "FileWrite - defined")
+if not defined FileWrite (echo "FileWrite - not defined")
+rem Чтобы удалить переменную, в командной строке введите: 
+set FileWrite=
+if defined FileWrite (echo "FileWrite - defined")
+if not defined FileWrite (echo "FileWrite - not defined")
+::
+set FileWrite="%date% %time%.html"
+Echo %FileWrite%
+::Echo %FileWrite%
+TIMEOUT /T 10
+pause
