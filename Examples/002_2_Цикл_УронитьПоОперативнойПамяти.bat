00bdf78b13138afba253b8d8ef40293fa715c390
qwe <qwe>
2022-05-19 13:38:00 +0300

    +GitMan_V0

---
+++ Examples/002_2_Цикл_УронитьПоОперативнойПамяти.bat
@@ -0,0 +1,19 @@
+@echo off
+cd %~dp0
+	set i=0
+	:m_435765
+		set Var%i%=%i%
+		call :getVar Var%i% b%
+		echo Var%i%=%b% 	
+		set /a i=i+1
+	if %i% LEQ 1000000000000000000000000 goto :m_435765
+pause
+exit /b 0
+rem  извлечение значения виртуальной переменной
+:getVar
+	call :getVarVar %%%1%% %2
+exit /b 0
+rem Разименовывание виртуальной переменной
+:getVarVar
+	set %2=%1
+exit /b 0
---
+++ GitMan_V0/Cd.bat
@@ -0,0 +1,6 @@
+@echo off
+cd %~dp0
+::echo %cd%
+cd ..\
+::echo %cd%
+TIMEOUT /T 5
