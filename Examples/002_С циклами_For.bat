010f4d1e35c14e6c7fb583fc4e2857a1a343d5df
qwe <qwe>
2022-05-19 13:35:27 +0300

    +GitMan_V0

--- Examples/002_С циклами_For.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd %~dp0
-for /L %%i in (1,1,10) do (echo i = %%i)
-echo.
-::цикл по всем файлам в папке
-for  %%f in (*) do (echo %%~nxf)
-echo.
-for  %%f in (*.txt *.exe) do (echo %%~nxf)
-pause
--- Examples/_17,03,2022_14,16,18,40_SibNout2020.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set qwe=4
-(set qwe=6)&&(echo %qwe%)
-(set qwe=8)&&(echo %qwe%)
-echo !! %qwe%
-echo !!! %%qwe%%
-pause
-exit /b
