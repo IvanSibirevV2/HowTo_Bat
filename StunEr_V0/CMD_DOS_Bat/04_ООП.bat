7cdac89e2781034dafcba08e5059d5fa889ec21f
IVS <IVS@IVS>
2023-03-23 15:48:11 +0300

    AutoCommit_2023.03.23_15:48:10_SibNout2020

---
+++ StunEr_V0/CMD_DOS_Bat/04_ООП.bat
@@ -0,0 +1,9 @@
+::Как работают классы?
+::Только это переменные имитирующие класс)
+::Типо как в JavaScript
+set class.A=1
+set class.B=2
+set class.C=HDSFHJS
+Echo %class.A%
+Echo %class.B%
+Echo %class.C%
