549efd74835ff09e0fa75a1ed683bc3ec8a68d13
IVS <IVS@IVS>
2023-05-07 16:31:28 +0300

    AutoCommit_2023.05.07_16:31:27_SibNout2020

---
+++ StunEr_V0/CMD_DOS_Bat/05_Функции/003_FunctionA1_Run.bat
@@ -0,0 +1,13 @@
+:::::::::::::::::::::::::::::::::
+@echo off
+cd %~dp0
+start notepad++ %0 
+:::::::::::::::::::::::::::::::::
+	call :Main
+pause
+exit /b
+:Main
+	003_FunctionA1.bat ":Main"
+	::Ну или так
+	:: call "005_FunctionA1.bat" ":Main"
+exit /b
