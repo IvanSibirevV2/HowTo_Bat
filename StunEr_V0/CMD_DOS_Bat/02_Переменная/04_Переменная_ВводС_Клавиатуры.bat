a7eaa033ebb80b56d88c5773ae94f0890bd2bbc6
IVS <IVS@IVS>
2023-05-07 15:38:28 +0300

    AutoCommit_2023.05.07_15:38:27_SibNout2020

--- StunEr_V0/CMD_DOS_Bat/02_Переменная/04_Переменная_ВводС_Клавиатуры.bat
+++ StunEr_V0/CMD_DOS_Bat/02_Переменная/04_Переменная_ВводС_Клавиатуры.bat
@@ -1,13 +1,5 @@
 start notepad++ %0
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-rem Чтобы добавить переменную, в командной строке введите
-set FileWrite="%date% %time%.html"
-::Если переменная существует
-if defined FileWrite (echo "FileWrite - defined")
-::Если переменная не существует
-if not defined FileWrite (echo "FileWrite - not defined")
-::Удаляем переменную
-set FileWrite=
-if defined FileWrite (echo "FileWrite - defined")
-if not defined FileWrite (echo "FileWrite - not defined")
+rem Ввод переменной с клавиатуры с приглашением на ввод
+Set /P Input=  Введите пароль для продолжения ^>
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::pause
