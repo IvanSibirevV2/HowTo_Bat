33bddba4c93f2b80f646ff6c74c5d8a4d2c11fee
IVS <IVS@IVS>
2023-07-02 22:56:10 +0300

    AutoCommit_2 июля 2023 г. 22:56:10_SibNout2020

--- GitMan_V3/GitMan_V4.bat
+++ GitMan_V3/GitMan_V4.bat
@@ -1,3 +1,3 @@
-start C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoExit -command "git add .;git status;echo !!!;echo AutoCommit_$((Get-Date).DateTime)_$($Env:UserName);echo !!!;git commit -m AutoCommit_$((Get-Date).DateTime)_$($Env:UserName);git log -3;echo git log ......;git push;TIMEOUT /T 5;Exit;"
+start C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoExit -command "git add .;git status;echo 0!!!;git commit -m AutoCommit_$((Get-Date).DateTime)_$($Env:UserName);echo 1!!!;git log -3;echo git_log_......;git push;TIMEOUT /T 5;Exit;"
 
 
