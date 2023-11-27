e75d3022117ca531be27db0a39ac6e4bc921467c
йцу <>
2023-11-27 09:22:39 +0300

    AutoCommit_27 ноября 2023 г. 9:22:39_SibNout2023

--- Examples_CMD/_000_0_Test.bat
+++
@@ -1,9 +0,0 @@
-:::::::::::::::::::::::::::::::::::::::::::::
-@echo off
-if "%cd%\" EQU "%~dp0" start notepad++ %0 
-cd %~dp0
-:::::::::::::::::::::::::::::::::::::::::::::
-set qwe=dsa;asda;
-echo %qwe%
-pause
-exit /b
