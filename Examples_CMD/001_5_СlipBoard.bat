53b3a1dfa0a9c316e9609ce447fbca34968d4cc2
йцу <>
2024-01-11 14:09:19 +0300

    AutoCommit_11 января 2024 г. 14:09:19_SibNout2023

---
+++ Examples_CMD/001_5_СlipBoard.bat
@@ -0,0 +1,21 @@
+:::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+if "%cd%\" EQU "%~dp0" ((start notepad++ %0)&&(exit /b))
+cd %~dp0
+title %~0 
+:::::::::::::::::::::::::::::::::::::::::::::
+set FileName="_%date:.=,%_%time::=,%_%username%.bat"
+set NL=^& echo.
+(
+echo @echo off %NL%^
+echo HelloWorld %NL%^
+echo Привет мир %NL%^
+pause %NL%^
+exit /b
+)>> %FileName%
+:::::::::::::::::::::::::::::::
+CLIP < %FileName%
+notepad %FileName%
+del %FileName%
+pause
+exit /b
