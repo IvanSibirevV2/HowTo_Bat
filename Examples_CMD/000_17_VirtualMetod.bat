067811b258555a9c0d1fc0b2b01cbd83a0706feb
йцу <>
2024-01-16 11:57:37 +0300

    AutoCommit_16 января 2024 г. 11:57:37_SibNout2023

---
+++ Examples_CMD/000_17_VirtualMetod.bat
@@ -0,0 +1,30 @@
+:::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+if "%cd%\" EQU "%~dp0" ((start notepad++ %0)&&(exit /b))
+cd %~dp0
+title %~0 
+:::::::::::::::::::::::::::::::::::::::::::::
+set NL=^& echo.
+set VirMet=(%NL%^
+set EAX=0%NL%^
+set EBX=0%NL%^
+)
+echo _%VirMet%_
+
+::%VirMet%
+set ECX=0
+set EDX=0
+
+ECHO EAX=%EAX%
+ECHO EBX=%EBX%
+ECHO ECX=%ECX%
+ECHO EDX=%EDX%
+::set NL=^& echo.
+::echo Многострочный %NL%^
+::Привет %NL%^
+::Мир
+
+
+:::::::::::::::::::::::::::::::::::::::::::::
+(TIMEOUT /T 1)&&(pause)&&(exit /b)
+:::::::::::::::::::::::::::::::::::::::::::::
