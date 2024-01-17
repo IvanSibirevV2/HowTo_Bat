f018224c3b0cc32c9dc264f5ecd5525d82f6043c
йцу <>
2024-01-17 11:09:29 +0300

    AutoCommit_17 января 2024 г. 11:09:29_SibNout2023

--- Examples_CMD/000_14_ПереходВИнтерактивныйРежим.bat
+++ Examples_CMD/000_14_ПереходВИнтерактивныйРежим.bat
@@ -1,24 +1,16 @@
 :::::::::::::::::::::::::::::::::::::::::::::
-goto initEnd
-call :init %0
-:init
 @echo off
 if "%cd%\" EQU "%~dp1" ((start notepad++ %1)&&(exit))
 cd %~dp1
 title %~1
 setlocal EnableDelayedExpansion
 cls
-exit /b
-:initEnd
 :::::::::::::::::::::::::::::::::::::::::::::
-call :init %0
-:::::::::::::::::::::::::::::::::::::::::::::
-echo %cd%
-CD ..\
-Echo %CD%
 Echo ****************************
 Echo cmd.exe will be started
 Echo Await and send _exit_
+::Обычно ставят ключ i
+::Ноесли Вы пишете батник прокатит и так
 cmd.exe
 :::::::::::::::::::::::::::::::::::::::::::::
 (TIMEOUT /T 1)&&(pause)&&(exit /b)
