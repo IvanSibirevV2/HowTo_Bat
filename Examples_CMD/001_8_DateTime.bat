b8a2802110669e24936e1d5fbe19aa652298a624
йцу <>
2024-01-18 13:22:13 +0300

    AutoCommit_18 января 2024 г. 13:22:13_SibNout2023

---
+++ Examples_CMD/001_8_DateTime.bat
@@ -0,0 +1,20 @@
+:::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+if "%cd%\" EQU "%~dp0" ((start notepad++ %0)&&(exit))
+cd %~dp0
+title %~0
+setlocal EnableDelayedExpansion
+cls
+:::::::::::::::::::::::::::::::::::::::::::::
+echo DateTime.Now=%date%
+echo DateTime.Now.Year=%date:~-4%
+echo DateTime.Now.Month=%date:~3,2%
+echo DateTime.Now.Day=%date:~0,2%
+echo DateTime.Now.Hour=%TIME:~0,2%
+echo DateTime.Now.Minutes=%TIME:~3,2%
+echo DateTime.Now.Seconds=%TIME:~6,2%
+echo DateTime.Now.Tik=%TIME:~9%
+echo %username%
+:::::::::::::::::::::::::::::::::::::::::::::
+(TIMEOUT /T 10)&&(pause)&&(exit /b)
+:::::::::::::::::::::::::::::::::::::::::::::
