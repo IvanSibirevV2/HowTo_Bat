a8afee74d3d8a64f0b69693802d2e5c455dc84c8
йцу <>
2024-08-19 22:15:28 +0300

    AutoCommit_19 августа 2024 г. 22:15:28_SibNout2023

--- Examples_CMD/001_3_Menu.bat
+++ Examples_CMD/001_3_Menu.bat
@@ -4,45 +4,38 @@
 @echo off
 if "%cd%\" EQU "%~dp0" ((start notepad++ %0)&&(exit))
 cd %~dp0
-title %~0
-setlocal EnableDelayedExpansion
+::::::::::::::::::::::::::::::::::::::::::::
+::Обьявление начала меню
+set /a m205019082024=2
+:begin_205019082024
 cls
-:::::::::::::::::::::::::::::::::::::::::::::
-call :MenuEngine :Mark1 :Mark2 :Mark3
+echo ErrorLevel=%ErrorLevel%
+::Отрисовка пунктов меню
+(if %m205019082024% EQU 0 (<nul set /p strTemp=*))&(echo Выход)
+(if %m205019082024% EQU 1 (<nul set /p strTemp=*))&(echo Да)
+(if %m205019082024% EQU 2 (<nul set /p strTemp=*))&(echo Нет)
+(if %m205019082024% EQU 3 (<nul set /p strTemp=*))&(echo Сомневаюсь)
+::Управляющие структуры
+set /a m205019082024m=3
+choice /c wse
+if %ErrorLevel% EQU 1 echo 1 w
+if %ErrorLevel% EQU 1 (set /a m205019082024=%m205019082024%-1)
+if %ErrorLevel% EQU 1 if %m205019082024% LSS 0 set /a m205019082024=%m205019082024m%
+if %ErrorLevel% EQU 1 goto :begin_205019082024
+if %ErrorLevel% EQU 2 echo 2 s
+if %ErrorLevel% EQU 2 (set /a m205019082024=%m205019082024%+1)
+if %ErrorLevel% EQU 2 if %m205019082024% GTR %m205019082024m% set /a m205019082024=0
+if %ErrorLevel% EQU 2 goto :begin_205019082024
+::Вшитый функционал
+if %ErrorLevel% EQU 3 if %m205019082024% EQU 0 echo Выход
+if %ErrorLevel% EQU 3 if %m205019082024% EQU 1 echo Да
+if %ErrorLevel% EQU 3 if %m205019082024% EQU 1 goto :begin_205019082024
+if %ErrorLevel% EQU 3 if %m205019082024% EQU 2 echo Нет
+if %ErrorLevel% EQU 3 if %m205019082024% EQU 2 goto :begin_205019082024
+if %ErrorLevel% EQU 3 if %m205019082024% EQU 3 echo Сомневаюсь
+if %ErrorLevel% EQU 3 if %m205019082024% EQU 3 goto :begin_205019082024
+::Самостоятельный выход из меню
+if %ErrorLevel% EQU 3 echo 3 e
 :::::::::::::::::::::::::::::::::::::::::::::
 (TIMEOUT /T 1)&&(pause)&&(exit /b)
 :::::::::::::::::::::::::::::::::::::::::::::
-:MenuEngine
-setlocal
- set _id=0
- :MenuEngine_begin
-  set _counter=0
-  set _target=
-  ::Вывод на экран
-  for %%i in (%*) do ((call %%i)&(set /a _counter=!_counter!+1))
-  choice /c ews  
-  if %ErrorLevel% EQU 2 ((echo w)&(set /a _id= %_id%-1)&(set %ErrorLevel%=0))
-  if %ErrorLevel% EQU 3 ((echo s)&(set /a _id= %_id%+1)&(set %ErrorLevel%=0))
-  if %_id% LSS 0 set /a _id=%_counter%-1
-  if %_id% GEQ %_counter% set _id=0
-  set _counter=%_id% 
-  if %ErrorLevel% EQU 1 (call %_target% do)
- goto :MenuEngine_begin
-endlocal
-exit /b
-
-:Mark1
-if "%1" EQU "" (if "%_id%" EQU "%_counter%" ((<nul set /p strTemp=*)&(set _target=%0))
- echo %_counter%. %0. Exit
-)else (if %_id% EQU %_counter% ((TIMEOUT /T 2)&(Exit)))
-exit /b
-:Mark2
-if "%1" EQU "" (if "%_id%" EQU "%_counter%" ((<nul set /p strTemp=*)&(set _target=%0))
- echo %_counter%. %0. Menu_2
-)else (if %_id% EQU %_counter% (echo ________Menu_1)&(pause))
-exit /b
-:Mark3
-if "%1" EQU "" (if "%_id%" EQU "%_counter%" ((<nul set /p strTemp=*)&(set _target=%0))
- echo %_counter%. %0. Menu_3
-)else (if %_id% EQU %_counter% (echo ________Menu_2)&(pause))
-exit /b
